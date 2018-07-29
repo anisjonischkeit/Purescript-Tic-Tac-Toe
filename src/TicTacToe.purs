e33f09cbfa11d38d33893d42b5956f8b69b64593
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-07-30 02:01:40 +1000

    cleanup

--- src/TicTacToe.purs
+++ src/TicTacToe.purs
@@ -18,10 +18,55 @@ instance showPiece :: Show Piece where
     X -> "X"
     O -> "O"
 
+type Tile = Maybe Piece
+type Row = Array Tile
+type Board = Array Row
+
+type Position = { x :: Int, y :: Int }
+
+data PlacingError
+  = NonExistantBoardPosition 
+  | NonEmptyTileExistsAtPosition
+
+initialBoard :: Board
+initialBoard =
+  [ [Nothing, Nothing, Nothing]
+  , [Nothing, Nothing, Nothing]
+  , [Nothing, Nothing, Nothing]
+  ]
+
+place :: Board -> Position -> Piece -> Either PlacingError Board
+place board pos piece = newBoard
+  where
+    row :: Maybe Row
+    row = board !! pos.y
+
+    newRow :: Either PlacingError Row
+    newRow = case row of 
+      Nothing -> Left NonExistantBoardPosition
+      Just r -> 
+        case r !! pos.x of 
+          Nothing -> Left NonExistantBoardPosition
+          Just (Just _) -> Left NonEmptyTileExistsAtPosition
+          Just Nothing -> 
+            case updateAt pos.x (Just piece) r of
+              Nothing -> Left NonExistantBoardPosition
+              Just nr -> Right nr
+      
+    newBoard :: Either PlacingError Board
+    newBoard = newRow >>= \nr -> 
+      case updateAt pos.y nr board of
+        Nothing -> Left NonExistantBoardPosition
+        Just nb -> Right nb
+
 invertPiece :: Piece -> Piece
 invertPiece = case _ of
   X -> O
   O -> X
+
+
+-- BoardState
+
 
 data BoardState
   = Playing
@@ -94,49 +139,3 @@ getBoardState board =
   then Tie
 
   else Playing
-
-type Tile = Maybe Piece
-type Row = Array Tile
-type Board = Array Row
-
-type Position = { x :: Int, y :: Int }
-
-data PlacingError
-  = NonExistantBoardPosition 
-  | NonEmptyTileExistsAtPosition
-
-initialBoard :: Board
-initialBoard =
-  [ [Nothing, Nothing, Nothing]
-  , [Nothing, Nothing, Nothing]
-  , [Nothing, Nothing, Nothing]
-  ]
-
-place :: Board -> Position -> Piece -> Either PlacingError Board
-place board pos piece = newBoard
-  where
-    row :: Maybe Row
-    row = board !! pos.y
-
-    newRow :: Either PlacingError Row
-    newRow = case row of 
-      Nothing -> Left NonExistantBoardPosition
-      Just r -> 
-        case r !! pos.x of 
-          Nothing -> Left NonExistantBoardPosition
-          Just (Just _) -> Left NonEmptyTileExistsAtPosition
-          Just Nothing -> 
-            case updateAt pos.x (Just piece) r of
-              Nothing -> Left NonExistantBoardPosition
-              Just nr -> Right nr
-      
-    newBoard :: Either PlacingError Board
-    newBoard = newRow >>= \nr -> 
-      case updateAt pos.y nr board of
-        Nothing -> Left NonExistantBoardPosition
-        Just nb -> Right nb
-
-
-
-test :: String
-test = "hiiisssi"
